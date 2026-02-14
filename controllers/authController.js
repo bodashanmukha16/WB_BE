dff48498d4e1520261852656a3aed78f25b58df2
bodashanmukha16 <bodashanmukha16@gmail.com>
2026-02-15 00:26:22 +0530

    V1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,36 +1,44 @@
-import bcrypt from "bcrypt"
-import generateToken from "../utils/generateToken.js"
-import User from '../config/db.js';
-export const login = async (req,res)=>{
-    const {username, password} = req.body
-    try{
-        const userResult = await User.findOne({ username });
-        if(userResult.rows.length === 0)
-        {
-            return res.status(400).json({message: "userNot found"
-            })
-        }
-        const user = userResult.rows[0]
-        const isMatch = await bcrypt.compare(password, user.password)
+import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/User.js"; // ⚠ correct import
 
-        if(!isMatch)
-        {
-            return res.status(400).json({message:"Invalid Credentials"})
-        }
-        const token = generateToken(user)
-        res.json({
-            message:"Login Successfull",
-            token,
-            user:{
-                id:user.id,
-                username: user.username,
-                role: user.role
-            }
-        })
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Mongo returns document or null
+    const user = await User.findOne({ username });
+    console.log(user);
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found"
+      });
     }
-    catch(e)
-    {
-        console.error(e)
-        res.status(500).json({message:"500 Server Not Ready"})
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
     }
+
+    const token = generateToken(user);
+
+    return res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    console.error("Login error:", error.message);
+    return res.status(500).json({
+      message: "Server error"
+    });
+  }
 };
